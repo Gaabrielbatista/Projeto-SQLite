66b74116ba6ec42006326ed8309ad70699f5361d
Gaabrielbatista <ggabrielbattista@gmail.com>
2025-09-22 19:36:45 -0300

    Função tabulate para exibir os dados

--- src/main.py
+++ src/main.py
@@ -1,4 +1,12 @@
-from database import *
+from database import criar_tabela, inserir_carro, listar_carros
+from tabulate import tabulate
+from os import system
 
-criar_conexao()
+system("cls")
+
 criar_tabela()
+inserir_carro("Honda Civic", 2020, "Prata")
+inserir_carro("Audi A6", 2022, "Preto")
+
+exibir_dados = (tabulate(listar_carros(), tablefmt='fancy_grid', headers=['ID', 'Modelo', 'Ano', 'Cor']))
+print(exibir_dados)
