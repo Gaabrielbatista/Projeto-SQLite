daae54a4f05af3a34af4752ed6f07d48ec8a16fa
Gaabrielbatista <ggabrielbattista@gmail.com>
2025-09-22 20:28:40 -0300

    função exibir em main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-from database import criar_tabela, inserir_carro, listar_carros
+from database import *
 from tabulate import tabulate
 from os import system
 
 system("cls")
 
+# Mostrar tabela
+def exibir():
+    exibir_dados = (tabulate(listar_carros(), tablefmt='fancy_grid', headers=['ID', 'Modelo', 'Ano', 'Cor']))
+    print(exibir_dados)
+
+
 criar_tabela()
 inserir_carro("Honda Civic", 2020, "Prata")
 inserir_carro("Audi A6", 2022, "Preto")
 
-exibir_dados = (tabulate(listar_carros(), tablefmt='fancy_grid', headers=['ID', 'Modelo', 'Ano', 'Cor']))
-print(exibir_dados)
+exibir()
+
+atualizar_carro(2, modelo="Toyota Corolla")
+atualizar_carro(4, cor="Azul")
+atualizar_carro(6, ano="2023")
+
+exibir()
