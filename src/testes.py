124e3f86eb39b76de2c8761abe39665b101350df
Gaabrielbatista <ggabrielbattista@gmail.com>
2025-10-07 22:55:20 -0300

    Criando interface, inserir e atualizar carro foi concluído.

--- src/testes.py
+++ src/testes.py
@@ -1,6 +1,6 @@
 from database import *
 from tabulate import tabulate
-
+from utils import limpar_terminal
 # Mostra tabela
 def exibir_dados():
     exibição = (tabulate(listar_carros(), tablefmt='fancy_grid', headers=['ID', 'Modelo', 'Ano', 'Cor']))
@@ -8,10 +8,10 @@ def exibir_dados():
 
 # Cria tabela
 criar_tabela()
+limpar_terminal()
+# inserir_carro("  asdasdeq", 1900, "cor aleatória")
+# exibir_dados()
 
-inserir_carro("  asdasdeq", 1900, "cor aleatória")
-exibir_dados()
-
-atualizar_carro(2, cor="cor aí")
+atualizar_carro(1, cor="  ")
 
 exibir_dados()
