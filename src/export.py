809c7993bf0a07e3fa326f7e8d463bf15bda8d6a
Gaabrielbatista <ggabrielbattista@gmail.com>
2025-12-15 23:13:36 -0300

    feat: If da função removido e conexão no lugar.

--- src/export.py
+++ src/export.py
@@ -6,18 +6,15 @@ from datetime import datetime
 hoje = datetime.now().today()
 
 def exportar_csv():
-    if not criar_conexao():
-        print("Erro: Não foi possível conectar ao banco de dados.")
-        return
+    # Conectar ao banco de dados e buscar os dados
+    conn = criar_conexao()
+    cur = conn.cursor()
     
     with open(f'exports/export_carros_{hoje.strftime("%d-%m-%Y")}.csv', mode='w', newline='', encoding='utf-8') as arquivo_csv:
         escritor_csv = csv.writer(arquivo_csv)
-
         # Escrever o cabeçalho
         escritor_csv.writerow(['ID', 'Modelo', 'Ano', 'Cor'])
-        # Conectar ao banco de dados e buscar os dados
-        conn = criar_conexao()
-        cur = conn.cursor()
+        
         cur.execute("SELECT * FROM Carros")
         dados_carros = cur.fetchall()
         
